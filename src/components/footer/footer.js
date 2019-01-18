76a09f26b343ea1eb84ad2c23896096c009f7f3e
Elena Golubovich <elenaholubovich988@gmail.com>
2019-01-18 17:02:59 +0300

    create_head&foot

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,8 +6,30 @@ import "./footer.scss";
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                Footer
+            <div className="footer-main">
+                <div className="footer-panel">
+                    <div className="logo-ft"></div>
+                    <div className="ul-menu">
+                        <ul>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Become a partner</a></li>
+                            <li><a href="#">Contacts</a></li>
+                        </ul>
+                    </div>
+                    <div className="icons">
+                        <i class="fab fa-vk fa-2x"></i>
+                        <i class="fab fa-facebook-f fa-2x"></i>
+                        <i class="fab fa-instagram fa-2x"></i>
+                        <i class="fab fa-twitter fa-2x"></i>
+                    </div>
+                </div>
+                <div className="ft-bt">
+                    <p>
+                        <i class="far fa-info-circle fa-xs"></i>
+                     2019 OOO"Charitable Aid Fund" YNP 1234678
+                    </p>
+
+                </div>
             </div>
         )
     }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,8 +7,27 @@ export default class Header extends Component {
     render() {
         return (
             <div className="header-main">
+                <div className="box-panel">
                 <div className="logo"></div>
-                Header
+                <div className="ul-menu">
+                    <ul>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Become a partner</a></li>
+                    <li><a href="#">Contacts</a></li>
+                    </ul>
+                </div>
+                    <div className="help">Together we help!</div>
+                    <div className="description">Brief description of the project</div>
+                    <div className="button-catalog">
+                        <p className="catalog">Catalog</p>
+                        <p className="services">Services</p>
+                    </div>
+                    <div className="cabinet">
+                        <i class="fas fa-user-alt" aria-hidden="true"></i>
+                        <p> Personal Area</p>
+                    </div>
+
+                </div>
             </div>
         )
     }
