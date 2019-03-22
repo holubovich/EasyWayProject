0fc4fb55d4c8bde6ad6375b256f5d020bc8e1c24
Elena Golubovich <elenaholubovich988@gmail.com>
2019-03-22 12:07:49 +0300

    add description to the personal area

--- src/components/pages/cabinet/cabinet.js
+++ src/components/pages/cabinet/cabinet.js
@@ -6,8 +6,17 @@ export default class Cabinet extends Component {
     render() {
         return (
             <div className="body-about">
-
-<HospiceList/>
+                <div className="page-about">
+                    <a href="/" title="Back to the main page"> Main</a> / Personal Area
+                </div>
+                <div className="main-wave">
+                    <div className="wave left"></div>
+                    <div className="wave-text">Description:</div>
+                    <div className="wave right"></div>
+                </div>
+                <p>In the course of its activities, the company must make a number of decisions: what product or product line should be produced and sold; what markets to enter with this product and how to strengthen its position in the market; how to choose the optimal production technology; what materials to purchase and how to use them; how to distribute available models and financial resources; what indicators of its activity does the enterprise prefer (should) achieve in relation to the technical characteristics of the goods produced, its quality, production efficiency.</p>
+                <div className="button"><a href="#">Choose a charity project</a></div>
+                <HospiceList/>
             </div>
         )
     }
